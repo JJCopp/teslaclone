9d9a88e95c53d4bef10cbb053f6a79dd8ecbf3e4
James Coppinger <jcoppingerj@gmail.com>
2022-01-26 10:47:57 -0700

    First commit

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import carSlice from '../features/car/carSlice';
+import carReducer from "../features/car/carSlice"
+
+export const store = configureStore({
+  reducer: {
+    car: carReducer
+  },
+});
